5305b8a23968f89292ada9e346c0fa4175268241
David Kim <limious333@gmail.com>
2024-11-03 10:17:12 -0500

    Added state machine and prevent jumping when in jumping state

--- Assets/scripts/ChracterMovement.cs
+++ Assets/scripts/ChracterMovement.cs
@@ -1,34 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
+enum PlayerState
+{
+    Idle,
+    Moving,
+    Jumping
+}
 
 public class ChracterMovement : MonoBehaviour
 {
-    public Rigidbody2D Rigidbody;
-    [SerializeField] private float moveSpeed = 100f;
-    public float horizontal;
-    public float vertical;
+
+    public float moveSpeed = 5f;
+    public float jumpForce = 20f;
+
+    private Rigidbody2D Rigidbody;
+    private float horizontal;
+    private float vertical;
+    private PlayerState currentState = PlayerState.Idle;
+
     // Start is called before the first frame update
     void Start()
     {
-        Rigidbody = GetComponent <Rigidbody2D>();
-        
+        Rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
-        if(Input.GetKeyDown(KeyCode.W) )
-        {
-            Rigidbody.AddForce(Vector2.up * moveSpeed, ForceMode2D.Impulse);
-        }
+        HandleJump();
     }
 
     private void FixedUpdate()
     {
-        Rigidbody.linearVelocity = new Vector2(horizontal * moveSpeed, Rigidbody.linearVelocityY);
-        
+        switch (currentState)
+        {
+            case PlayerState.Idle:
+            case PlayerState.Moving:
+            case PlayerState.Jumping:
+                HandleMovement();
+                break;
+        }
     }
 
+    private void HandleMovement()
+    {
+        if (Mathf.Abs(horizontal) > 0.01f)
+        {
+            transform.position += new Vector3(horizontal * moveSpeed * Time.deltaTime, 0, 0);
+            if (currentState != PlayerState.Jumping)
+            {
+                currentState = PlayerState.Moving;
+            }
+        }
+        else if (currentState == PlayerState.Moving)
+        {
+            currentState = PlayerState.Idle;
+        }
+    }
+
+    private void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && currentState != PlayerState.Jumping)
+        {   
+            currentState = PlayerState.Jumping;
+            Rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            if (currentState == PlayerState.Jumping)
+            {
+                currentState = PlayerState.Idle;
+            }
+        }
+    }
 }
